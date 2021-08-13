4982947be94a92c063581e853019addcdf0297ab
Steve Markgraf <steve@steve-m.de>
2021-08-14 00:03:33 +0200

    fix build for Kernel 5.13
    This fixes the following error:
    ERROR: modpost: "irq_to_desc" [i2c-ch341-usb.ko] undefined!
    
    irq_to_desc() is an internal function and is not exported anymore
    by recent Kernels.

--- i2c-ch341-usb.c
+++ i2c-ch341-usb.c
@@ -633,6 +633,8 @@ static int ch341_irq_probe (struct ch341_device* ch341_dev)
 {
     int i;
     int result;
+    struct irq_data *irqdata = NULL;
+
 
     CHECK_PARAM_RET (ch341_dev, -EINVAL);
 
@@ -657,7 +659,10 @@ static int ch341_irq_probe (struct ch341_device* ch341_dev)
 
     for (i = 0; i < ch341_dev->irq_num; i++)
     {
-        ch341_dev->irq_descs[i]   = irq_to_desc(ch341_dev->irq_base + i);
+        irqdata = irq_get_irq_data(ch341_dev->irq_base + i);
+        if (irqdata)
+            ch341_dev->irq_descs[i]   = irq_data_to_desc(irqdata);
+
         ch341_dev->irq_enabled[i] = false;
 
         irq_set_chip          (ch341_dev->irq_base + i, &ch341_dev->irq);
